8e8b67be8f550ee9806b9f643e0aba3cc90e5d58
Chandrashekhar Patil <chandrashekhar.patil@springer.com>
2016-08-02 10:58:05 +0530

    Updated

--- src/main/java/com/db/controllers/ShopController.java
+++ src/main/java/com/db/controllers/ShopController.java
@@ -3,7 +3,10 @@ package com.db.controllers;
 import com.db.models.Shop;
 import com.db.services.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,17 +19,17 @@ public class ShopController {
     private ShopService shopService;
     Shop shop = new Shop();
 
-    @RequestMapping(value = "/all", method=RequestMethod.GET)
+    @RequestMapping(value = "/all", method=RequestMethod.GET, produces = "application/json")
     public List<Shop> getAll() {
         return shopService.getAll();
     }
 
-    @RequestMapping(method=RequestMethod.POST)
-    public String create(@RequestBody String payload) {
-        return shopService.create(shop.fromJson(payload)).toJson();
-    }
+    @RequestMapping(method=RequestMethod.POST, produces = "application/json", consumes="application/json")
+    public Shop create(@RequestBody String payload) { return shopService.create(shop.fromJson(payload)); }
+    //@RequestMapping(method=RequestMethod.POST, produces = "application/json")
+    //public String create(@RequestBody String payload) { return shopService.create(shop.fromJson(payload)).toJson(); }
 
-    @RequestMapping(value = "/nearby", method=RequestMethod.POST)
+    @RequestMapping(value = "/nearby", method=RequestMethod.POST, produces = "application/json")
     public List<Shop> nearBy(@RequestBody String payload) throws IOException {
         return shopService.nearBy(shop.fromJson(payload));
     }
